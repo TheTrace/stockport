a9734bbb66aa9cd7dfe35c926c90da7a17c70e1f
Alan Tracey <actracey@gmail.com>
2014-02-28 13:04:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockport::Application.config.secret_key_base = '4cf7b2bcf46d672e2e7c1ce53659e658d9db48a57566031918883479956db779bd96240ccb0371f999bf75313469be34c45f858a7cddfd30b6edd50d82053d37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stockport::Application.config.session_store :cookie_store, key: '_stockport_session'
