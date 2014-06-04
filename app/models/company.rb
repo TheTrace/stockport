1d7f7a7d4fdbe093af311cc05201fb18c5cad1cb
Alan Tracey <actracey@gmail.com>
2014-06-04 14:07:31 +0100

    Some companies and transactions stuff

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,8 @@
 class Company < ActiveRecord::Base
 	has_many :transactions
+
+	def self.for_select
+		Company.order("companies.name,companies.ticker").map{|c|[[c.ticker,c.name].join(" "),c.id]}
+	end 
+
 end
