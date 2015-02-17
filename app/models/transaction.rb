a3b69f7c3f0364e7a974f958268494354029fd7a
Alan Tracey <actracey@gmail.com>
2015-02-17 17:00:15 +0000

    Tweaks to transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -18,4 +18,35 @@ class Transaction < ActiveRecord::Base
 			ALL.map{|t|[NAMES[t],t]}
 		end
 	end
+  
+  def expenses
+    exp = 0.00
+    exp += commission.to_f if !commission.blank?
+    exp += stamp_duty.to_f if !stamp_duty.blank?
+    exp += self.PTM_levy.to_f if !self.PTM_levy.blank?
+    #transaction.commission + transaction.stamp_duty + transaction.PTM_levy
+    exp
+  end
+  
+  def total_cost
+    tc = 0.00
+    tc += consideration if !consideration.blank?
+    tc += expenses
+  end
+  
+  def avg_cost
+    if !total_cost.blank? && !quantity.blank? && quantity != 0
+      total_cost * 100 / quantity
+    else
+      0.00
+    end
+  end
+  
+  def percent_cost
+    if !expenses.blank? && !total_cost.blank? && total_cost != 0
+		  expenses * 100 / total_cost
+    else
+      0.00
+    end
+  end
 end
