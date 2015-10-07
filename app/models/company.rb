663af93a29f34eb0df0f9699f45c7a9db3d2dbbb
Alan Tracey <actracey@gmail.com>
2015-10-07 16:02:21 +0000

    Few formatting tweaks, for dropdowns etc

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,41 @@
 class Company < ActiveRecord::Base
 	has_many :transactions
 
+	class Bourse
+		LSE = "LSE"
+		NYSE = "NYSE"
+		NASDAQ = "NASDAQ"
+
+		NAMES = {
+			LSE => "London Stock Exchange",
+			NYSE => "New York Stock Exchange",
+			NASDAQ => "NASDAQ"
+			# National Association of Securities Dealers Automated Quotations
+		}
+
+		ALL = [LSE, NYSE, NASDAQ]
+
+		def self.for_select
+			ALL.map{|t|[NAMES[t],t]}
+		end
+	end
+  
+	class Country
+		UK = "United Kingdom"
+		US = "United States"
+
+		NAMES = {
+			UK => "United Kingdom",
+			US => "United States of America"
+		}
+
+		ALL = [UK, US]
+
+		def self.for_select
+			ALL.map{|t|[NAMES[t],t]}
+		end
+	end
+
 	def self.for_select
 		Company.order("companies.name,companies.ticker").map{|c|[[c.ticker,c.name].join(" "),c.id]}
 	end 
